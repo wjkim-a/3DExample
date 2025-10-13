beff81cf91ad4d3369dd9bc98f126ee9899f8557
wjkim-a <wj.kim.a@kiweb.or.kr>
2025-10-13 17:00:50 +0900

    [add] 파괴되는 오브젝트 추가

---
+++ Assets/Scripts/CreateNDestroy/SelfDestroyObj.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestroyObj : MonoBehaviour
+{
+    [SerializeField] private float _destroyDelay;
+    private float _aliveTime = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        CheckSelfDestroy();
+    }
+
+    private void CheckSelfDestroy()
+    {
+        if (_aliveTime >= _destroyDelay)
+            Destroy(gameObject);
+    }
+}
